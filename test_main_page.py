3c34719646ad4e3ccadb1123daa0c52ca38970d2
neo <eafifa07@yandex.ru>
2023-02-19 16:36:26 +0500

    added test files

---
+++ test_main_page.py
@@ -0,0 +1,12 @@
+import pytest
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    browser.get(link)
+    time.sleep(8)
+    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
+    login_link.click()
+    
